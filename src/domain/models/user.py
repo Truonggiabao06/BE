3de8492787a56706caee306365caf2868d1e874d
Gia Bao <baotg0966@ut.edu.vn>
2025-08-22 03:08:50 +0700

    feat(payments): add base, fake, and stripe-like payment adapters + update models

--- src/domain/models/user.py
+++ src/domain/models/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from infrastructure.databases.base import Base
 
 class User:
-    
+
     def __innit__(self, user_name: str, password: str, description: str = None, status: bool = True):
         self.user_name = user_name
         self.password = password
